352a12301e6ca158bcb0eef93a18e9395b36f5c6
John Grinwis <john@grinwis.com>
2024-11-26 17:09:02 +0100

    added two files

---
+++ constants.ts
@@ -0,0 +1,19 @@
+/*
+This is the only file that should be updated.
+It defines some constants including the JSON path to lookup the UNAME and PASSWD so can look like user.email for example.
+If you want to test with httpie: http POST https://api.grinwis.com/login user:='{"name":"test@test.nl","password":"test"}'
+*/
+
+// define the json fields where username and password can be found.
+export const UNAME = "username";
+export const PASSWD = "password";
+
+// our key generating and know password lookup subWorker endpoints
+export const KEY_GENERATOR_URL =
+  "https://nomoreleaks.grinwis.com/nomoreleakskey";
+export const KNOWN_KEY_URL = "https://nomoreleaks.grinwis.com/ew-knownkey";
+export const POSITIVE_MATCH_URL =
+  "https://nomoreleaks.grinwis.com/positiveMatch"; // case sensitive endpoint
+
+// header that's going to be forwarded to the origin
+export const NO_MORE_LEAKS_HEADER = "x-nomoreleaks";
